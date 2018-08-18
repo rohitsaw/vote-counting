c0c0da206e91c3b8a8dff15ea252e8c0aba4e16d
rohitsaw <rsaw409@gmail.com>
2018-08-18 15:57:11 +0530

    first released

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask, render_template, jsonify
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"]=os.getenv("SECRET_KEY")
+socketio=SocketIO(app)
+
+votes={"yes":0, "no":0, "maybe":0}
+
+@app.route("/")
+def index():
+    return render_template("index.html", votes=votes)
+
+@socketio.on("submit vote")
+def vote(data):
+    selection=data["selection"]
+    votes[selection]+=1
+    emit("vote totals", votes, broadcast=True)
